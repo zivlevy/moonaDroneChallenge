e8191654f6d00f53af9d530358bd0f84b6ed4ab3
Ziv Levy <zivilevy@gmail.com>
2016-03-15 00:15:06 +0200

    websocket

--- test.js
+++ test.js
@@ -1,32 +1,65 @@
- (function(ext) {
-  // Cleanup function when the extension is unloaded
-  ext._shutdown = function() {};
-  
-  // Status reporting code
-  // Use this to report missing hardware, plugin or unsupported browser
-  ext._getStatus = function() {
-  return {status: 2, msg: 'Ready'};
-  };
-  
-  // Functions for block with type 'w' will get a callback function as the
-  // final argument. This should be called to indicate that the block can
-  // stop waiting.
-  ext.wait_random = function(callback) {
-  wait = Math.random();
-  console.log('Waiting for ' + wait + ' seconds');
-  window.setTimeout(function() {
-                    callback();
-                    }, wait*1000);
-  };
-  
-  // Block and block menu descriptions
-  var descriptor = {
-  blocks: [
-           ['w', 'takeoff', 'wait_random'],
-           ['w', 'LAND', 'wait_random'],
-           ]
-  };
-  
-  // Register the extension
-  ScratchExtensions.register('Random wait extension', descriptor, ext);
-  })({});
+(function(ext) {
+ // Cleanup function when the extension is unloaded
+ ext._shutdown = function() {};
+ 
+ // Status reporting code
+ // Use this to report missing hardware, plugin or unsupported browser
+ ext._getStatus = function() {
+ return {status: 2, msg: 'Ready'};
+ };
+ 
+ // Functions for block with type 'w' will get a callback function as the
+ // final argument. This should be called to indicate that the block can
+ // stop waiting.
+ ext.wait_random = function(callback) {
+ wait = Math.random();
+ console.log('Waiting for ' + wait + ' seconds');
+ window.setTimeout(function() {
+                   callback();
+                   }, wait*1000);
+ };
+ 
+ ext.WebSocketTest()
+ {
+    if ("WebSocket" in window)
+        {
+            alert("WebSocket is supported by your Browser!");
+ 
+             // Let us open a web socket
+             var ws = new WebSocket("ws://localhost:9998");
+             
+             ws.onopen = function()
+             {
+             // Web Socket is connected, send data using send()
+             ws.send("Message to send");
+             alert("Message is sent...");
+             };
+             
+             ws.onmessage = function (evt)
+             {
+             var received_msg = evt.data;
+             alert("Message is received...");
+             };
+             
+             ws.onclose = function()
+             {
+                // websocket is closed.
+                alert("Connection is closed...");
+            }
+    } else {
+     // The browser doesn't support WebSocket
+     alert("WebSocket NOT supported by your Browser!");
+     }
+ };
+ 
+ // Block and block menu descriptions
+ var descriptor = {
+ blocks: [
+          ['w', 'takeoff', 'WebSocketTest'],
+          ['w', 'LAND', 'wait_random'],
+          ]
+ };
+ 
+ // Register the extension
+ ScratchExtensions.register('Random wait extension', descriptor, ext);
+ })({});
