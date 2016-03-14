bbf51631f7d1dc101d1f442002411cc8f796ede1
Ziv Levy <zivilevy@gmail.com>
2016-03-14 08:01:39 +0200

    q

--- test.js
+++ test.js
@@ -8,9 +8,23 @@
     };
     // Descriptions of the blocks and menus the extension adds
     var descriptor = {
-        blocks: [
-[' ', 'hello world'] 
-        ]
+blocks: [
+        ['w', 'turn motor on for %n secs',             'motorOnFor', 1],
+        [' ', 'turn motor on',                         'allMotorsOn'],
+        [' ', 'turn motor off',                        'allMotorsOff'],
+        [' ', 'set motor power %n',                    'startMotorPower', 100],
+        [' ', 'set motor direction %m.motorDirection', 'setMotorDirection', 'this way'],
+        ['h', 'when distance %m.lessMore %n',          'whenDistance', '<', 20],
+        ['h', 'when tilt %m.eNe %n',                   'whenTilt', '=', 1],
+        ['r', 'distance',                              'getDistance'],
+        ['r', 'tilt',                                  'getTilt']
+    ],
+    menus: {
+        motorDirection: ['this way', 'that way', 'reverse'],
+        lessMore: ['<', '>'],
+        eNe: ['=','not =']
+    },
+    url: 'http://info.scratch.mit.edu/WeDo'
     };
     // Register the extension
     ScratchExtensions.register('Hello World', descriptor, ext);
