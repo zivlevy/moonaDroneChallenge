0fc5169a1c75a002ae301e0406ee748238ab7520
Ziv Levy <zivilevy@gmail.com>
2016-03-16 05:39:01 +0200

    v1

--- test.js
+++ test.js
@@ -1,102 +1,82 @@
 (function(ext) {
- // Cleanup function when the extension is unloaded
- ext._shutdown = function() {
- ws.close();
- };
- 
- // Status reporting code
- // Use this to report missing hardware, plugin or unsupported browser
- ext._getStatus = function() {
- return {status: 2, msg: 'Ready'};
- };
- 
- // Functions for block with type 'w' will get a callback function as the
- // final argument. This should be called to indicate that the block can
- // stop waiting.
- ext.wait_random = function(callback) {
- wait = Math.random();
- console.log('Waiting for ' + wait + ' seconds');
- window.setTimeout(function() {
-                   callback();
-                   }, wait*1000);
- };
- 
- ext.WebSocketTest = function (callback)
- {
- alert('jjj');
-//    if ("WebSocket" in window)
-//        {
-//            alert("WebSocket is supported by your Browser!");
-// 
-//             // Let us open a web socket
-//             var ws = new WebSocket("ws://10.0.0.253:8000");
-//             
-//             ws.onopen = function()
-//             {
-//             // Web Socket is connected, send data using send()
-//             ws.send("3:100");
-//             alert("Message is sent...");
-//             };
-//             
-//             ws.onmessage = function (evt)
-//             {
-//             var received_msg = evt.data;
-//             alert("Message is received..." + received_msg);
-//             callback('true');
-//             };
-//             
-//             ws.onclose = function()
-//             {
-//                // websocket is closed.
-//                alert("Connection is closed...");
-//            }
-//    } else {
-//     // The browser doesn't support WebSocket
-//     alert("WebSocket NOT supported by your Browser!");
-//     }
- alert ('Takeoff sent');
- ws.send("3:100");
- ws.onmessage = function (evt)
- {
- var received_msg = evt.data;
- alert("Message is received inside takeoff..." + received_msg);
- callback('ACK');
- 
- };
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {
+        ws.close();
+    };
 
- };
- 
- // Block and block menu descriptions
- var descriptor = {
- blocks: [
-          ['R', 'takeoff', 'WebSocketTest'],
-          ['w', 'LAND', 'wait_random'],
-          ]
- };
- 
- // Register the extension
- ScratchExtensions.register('Moona Drone Challenge', descriptor, ext);
- console.log ('new ws');
- var ws = new WebSocket("ws://10.0.0.253:8000");
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
 
- 
- ws.onopen = function()
- {
-     alert("Socket open.");
- };
- 
- ws.onmessage = function (evt)
- {
- var received_msg = evt.data;
- alert("Message is received..." + received_msg);
+    // Functions for block with type 'w' will get a callback function as the
+    // final argument. This should be called to indicate that the block can
+    // stop waiting.
+    ext.land = function(callback) {
+        alert('Land sent');
+        ws.send('9');
+        ws.onmessage = function (evt)
+        {
+            var received_msg = evt.data;
+            if (received_msg=='ACK') {
+                callback('ACK');
+            } else {
+                callback('FAIL');
+            }
 
- };
- 
- ws.onclose = function()
- {
- // websocket is closed.
- alert("Connection is closed...");
- };
+        };
+    };
 
- 
- })({});
+    ext.takeoff = function (callback) {
+
+        alert ('Takeoff sent');
+        ws.send('3:100');
+        ws.onmessage = function (evt)
+        {
+            var received_msg = evt.data;
+            if (received_msg=='ACK') {
+                callback('ACK');
+            } else {
+                callback('FAIL');
+            }
+
+        };
+
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['w', 'TAKEOFF', 'takeoff'],
+            ['w', 'LAND', 'land']
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Moona Drone Challenge', descriptor, ext);
+    console.log ('new ws');
+    var ws = new WebSocket("ws://10.0.1.253:8000");
+
+
+    ws.onopen = function()
+    {
+
+        alert("Socket open.");
+    };
+
+    ws.onmessage = function (evt)
+    {
+        var received_msg = evt.data;
+        alert("Message is received..." + received_msg);
+
+    };
+
+    ws.onclose = function()
+    {
+        // websocket is closed.
+        alert("Connection is closed...");
+    };
+
+
+})({});
