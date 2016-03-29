ddcae13a9cea2825acb799a3d04bf6f3bdbd2274
Ziv Levy <zivilevy@gmail.com>
2016-03-29 13:17:02 +0300

    v16

--- mdc.js
+++ mdc.js
@@ -48,7 +48,7 @@
 // TAKOFF
     ext.takeoff = function (callback) {
         alert ('Takeoff sent');
-        ws.send('3:7');
+        ws.send('3:5');
         ws.onmessage = function (evt)
         {
             var received_msg = evt.data;
